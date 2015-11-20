307d096feb9e37a47643fe034103a1f2c6a78828
Jesse Pinho <jesse@jessepinho.com>
2015-11-19 20:58:22 -0600

    Create TilesViewController

---
+++ SocialMosaic/TilesViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  SocialMosaic
+//
+//  Created by Jesse Pinho on 11/19/15.
+//  Copyright © 2015 SocialMosaic. All rights reserved.
+//
+
+#import "TilesViewController.h"
+
+int const TilesPerRow = 10;
+
+@interface TilesViewController ()
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@end
+
+@implementation TilesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"TileCell" forIndexPath:indexPath];
+    return cell;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CGFloat cellSize = self.collectionView.frame.size.width / TilesPerRow;
+    return CGSizeMake(cellSize, cellSize);
+}
+@end
